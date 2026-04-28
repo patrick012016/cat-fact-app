f1378a1c2adbe69a965b836d233d9f7eb1607c18
Patryk Górniak <patryk012016@gmail.com>
2026-04-28 18:53:34 +0200

    feat: implement main class AppRunner and wire up dependency injection, configuration

--- src/CatFact.ConsoleApp/Program.cs
+++ src/CatFact.ConsoleApp/Program.cs
@@ -1,6 +1,12 @@
-﻿using CatFactApp.Configuration;
+﻿using CatFactApp.Clients;
+using CatFactApp.Configuration;
+using CatFactApp.Formatters;
+using CatFactApp.Models;
+using CatFactApp.Runners;
+using CatFactApp.Storage;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -10,5 +16,20 @@ builder.Services.AddOptions<CatFactConfig>()
     .ValidateDataAnnotations()
     .ValidateOnStart();
 
+builder.Services.AddSingleton<IFileService, FileService>();
+builder.Services.AddSingleton<ITextFormatter<CatFactModel>, CatFactFormatter>();
+builder.Services.AddTransient<AppRunner>();
+
+builder.Services.AddHttpClient<ICatFactClient, CatFactClient>((serviceProvider, client) =>
+{
+    var config = serviceProvider.GetRequiredService<IOptions<CatFactConfig>>().Value;
+    client.BaseAddress = new Uri(config.ApiUrl);
+
+    client.Timeout = TimeSpan.FromSeconds(5);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 using var host = builder.Build();
+
+var app = host.Services.GetRequiredService<AppRunner>();
+await app.RunAsync();
---
+++ src/CatFact.ConsoleApp/Runners/AppRunner.cs
@@ -0,0 +1,79 @@
+using CatFactApp.Clients;
+using CatFactApp.Formatters;
+using CatFactApp.Models;
+using CatFactApp.Storage;
+using Microsoft.Extensions.Logging;
+
+namespace CatFactApp.Runners;
+
+public class AppRunner
+{
+    private readonly ICatFactClient _catFactClient;
+    private readonly IFileService _fileService;
+    private readonly ILogger<AppRunner> _logger;
+    private readonly ITextFormatter<CatFactModel> _formatter;
+
+    public AppRunner(
+        ICatFactClient catFactClient,
+        IFileService fileService,
+        ILogger<AppRunner> logger, ITextFormatter<CatFactModel> formatter)
+    {
+        _catFactClient = catFactClient;
+        _fileService = fileService;
+        _logger = logger;
+        _formatter = formatter;
+    }
+
+    public async Task RunAsync(CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("Press [SPACE] to fetch a fact.");
+        Console.WriteLine("Press [Q] to quit.");
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(intercept: true).Key;
+
+                if (key == ConsoleKey.Q)
+                {
+                    _logger.LogInformation("Closing the application...");
+                    break;
+                }
+
+                if (key == ConsoleKey.Spacebar)
+                {
+                    await ProcessSingleFactAsync(cancellationToken);
+                }
+            }
+
+            await Task.Delay(100, cancellationToken);
+        }
+    }
+
+
+    private async Task ProcessSingleFactAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var fact = await _catFactClient.GetRandomFactAsync(cancellationToken);
+
+            if (fact is null)
+            {
+                _logger.LogWarning("Received an empty response from the API.");
+                return;
+            }
+
+            await _fileService.AppendFactAsync(fact, _formatter, cancellationToken);
+            _logger.LogDebug("Successfully appended data to the file.");
+        }
+        catch (IOException ex)
+        {
+            _logger.LogError("Cannot write to file. Details: {Message}", ex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex, "An unexpected error occurred in the main process.");
+        }
+    }
+}
