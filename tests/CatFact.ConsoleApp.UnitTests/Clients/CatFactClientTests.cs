f91098fad0f4cff278cb32d95f3d7d2ca8e3857e
Patryk Górniak <patryk012016@gmail.com>
2026-04-28 22:39:47 +0200

    test: add more unit tests for CatFactClient

--- tests/CatFact.ConsoleApp.UnitTests/Clients/CatFactClientTests.cs
+++ tests/CatFact.ConsoleApp.UnitTests/Clients/CatFactClientTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Text.Json;
 using CatFactApp.Clients;
@@ -10,6 +11,7 @@ using Xunit;
 
 namespace CatFactApp.Tests.Unit.Clients;
 
+[ExcludeFromCodeCoverage]
 public class CatFactClientTests
 {
     private readonly MockHttpMessageHandler _mockHttp;
@@ -40,7 +42,7 @@ public class CatFactClientTests
         var jsonResponse = JsonSerializer.Serialize(apiResponse);
 
         _mockHttp.Expect(HttpMethod.Get, _expectedRequestUrl)
-            .Respond("application/json", jsonResponse);
+            .Respond(HttpStatusCode.OK, "application/json", jsonResponse);
         var sut = CreateSut();
 
         // Act
@@ -51,6 +53,45 @@ public class CatFactClientTests
         Assert.Equal(expectedFact, result.Fact);
         Assert.Equal(expectedLength, result.Length);
         _mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Theory]
+    [InlineData("", 10)]
+    [InlineData("   ", 10)]
+    [InlineData("Test fact", 0)]
+    [InlineData("Test fact", -15)]
+    public async Task GetRandomFactAsync_WhenApiReturnsInvalidData_ShouldReturnNull(string invalidFact,
+        int invalidLength)
+    {
+        // Arrange
+        var apiResponse = new CatFactResponse(invalidFact, invalidLength);
+        var jsonResponse = JsonSerializer.Serialize(apiResponse);
+
+        _mockHttp.Expect(HttpMethod.Get, _expectedRequestUrl)
+            .Respond(HttpStatusCode.OK, "application/json", jsonResponse);
+        var sut = CreateSut();
+
+        // Act
+        var result = await sut.GetRandomFactAsync(CancellationToken.None);
+
+        // Assert
+        Assert.Null(result);
+        _mockHttp.VerifyNoOutstandingExpectation();
+    }
+
+    [Fact]
+    public async Task GetRandomFactAsync_WhenRequestIsCanceled_ShouldReturnNull()
+    {
+        // Arrange
+        var sut = CreateSut();
+        using var cancellationTokenSource = new CancellationTokenSource();
+        await cancellationTokenSource.CancelAsync();
+
+        // Act
+        var result = await sut.GetRandomFactAsync(cancellationTokenSource.Token);
+
+        // Assert
+        Assert.Null(result);
     }
 
     [Fact]
